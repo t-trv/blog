00b6c02156f9f3c3b0f49fdc890712d1f5e72c67
trvv <trhuvan@gmail.com>
2025-09-06 23:50:49 +0700

    install lib morgan

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/tin-tuc', (req, res) => {
-  res.send('Hello World!')
+// Log ra request
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1 style="color:red;">Xin chao anh em</h1>  
+  `)
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
